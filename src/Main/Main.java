2b5a6859103d17c96e58f447bf0bbb8f78760fcd
baharoon <mohammed.ba-haroon@ocsbstudent.ca>
2021-05-09 00:56:51 -0400

    Finally successful after endless trials!!!!

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,13 +5,44 @@
  */
 package Main;
 
+import java.util.*;
+
 /**
  *
  * @author k_zeb
  */
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter word:");
+        String word = in.nextLine().toUpperCase();
+
+        
+        //First Line:
+        System.out.print("* ");
+        for (int x=0;x<word.length(); x++){
+            System.out.print(word.charAt(x)+" ");
+        }
+        System.out.println("*");
+
+        //Middle Section:
+        for (int x=0;x<word.length();x++){
+            System.out.print(word.charAt(word.length()-(x+1))+" ");
+                for (int x=0;x<word.length();x++){
+                System.out.print("* ");
+                }
+            System.out.println(word.charAt(x));
+        
+        }
+        //Last Line:
+        System.out.print("* ");
+        for (int x=word.length()-1;x>=0;x--){
+            System.out.print(word.charAt(x)+" ");
+        }
+        System.out.println("*");
     }
-    
+
+    //Finally successful after endless trials!!!!
 }
